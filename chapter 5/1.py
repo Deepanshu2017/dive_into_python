4f2aebceff582b0070ec5c598cad959c09e43d8c
Deepanshu Thakur <deepanshu2017@hotmail.com>
2016-06-06 13:24:00 +0530

    Update 1.py

--- chapter 5/1.py
+++ chapter 5/1.py
@@ -2,5 +2,5 @@ class FirstClass:
     """Simple class with method say_hi"""
 
     def say_hi(self, name="anonymous"):
-        """Don't worry about `self`. We will come back to that soon"""
+        # Don't worry about `self`. We will come back to that soon
         print "Hi, " + name
